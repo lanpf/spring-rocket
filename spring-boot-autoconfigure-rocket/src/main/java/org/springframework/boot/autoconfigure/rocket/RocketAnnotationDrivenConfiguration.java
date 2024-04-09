f56e2943e7980e2fa2decd77fdf3026abed9b106
lanpf <lanxiaozhu2007@hotmail.com>
2024-04-09 17:38:22 +0800

    Spring Boot integration

---
+++ spring-boot-autoconfigure-rocket/src/main/java/org/springframework/boot/autoconfigure/rocket/RocketAnnotationDrivenConfiguration.java
@@ -0,0 +1,43 @@
+package org.springframework.boot.autoconfigure.rocket;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.rocket.annotation.EnableRocket;
+import org.springframework.rocket.annotation.RocketListenerAnnotationBeanPostProcessor;
+import org.springframework.rocket.client.RocketPushConsumerFactory;
+import org.springframework.rocket.config.DefaultRocketListenerContainerFactory;
+import org.springframework.rocket.config.RocketSupportBeanNames;
+import org.springframework.rocket.support.converter.MessagingMessageConverter;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass(EnableRocket.class)
+public class RocketAnnotationDrivenConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    DefaultRocketListenerContainerFactoryConfigurer rocketListenerContainerFactoryConfigurer(
+            ObjectProvider<RocketProperties> rocketProperties,
+            ObjectProvider<MessagingMessageConverter> messageConverter) {
+        DefaultRocketListenerContainerFactoryConfigurer configurer = new DefaultRocketListenerContainerFactoryConfigurer(rocketProperties.getIfUnique());
+        configurer.setMessageConverter(messageConverter.getIfUnique());
+        return configurer;
+    }
+
+    @Bean(name = RocketListenerAnnotationBeanPostProcessor.DEFAULT_ROCKET_LISTENER_CONTAINER_FACTORY_BEAN_NAME)
+    @ConditionalOnMissingBean(name = RocketListenerAnnotationBeanPostProcessor.DEFAULT_ROCKET_LISTENER_CONTAINER_FACTORY_BEAN_NAME)
+    DefaultRocketListenerContainerFactory rocketListenerContainerFactory(
+            DefaultRocketListenerContainerFactoryConfigurer configurer, ObjectProvider<RocketPushConsumerFactory> consumerFactory) {
+        DefaultRocketListenerContainerFactory containerFactory = new DefaultRocketListenerContainerFactory(consumerFactory.getIfAvailable());
+        configurer.configure(containerFactory);
+        return containerFactory;
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    @EnableRocket
+    @ConditionalOnMissingBean(name = RocketSupportBeanNames.ROCKET_LISTENER_ANNOTATION_PROCESSOR_BEAN_NAME)
+    static class EnableRocketConfiguration {
+    }
+}
--- spring-boot-autoconfigure-rocket/src/main/java/org/springframework/boot/autoconfigure/rocket/RocketProperties.java
+++ spring-boot-autoconfigure-rocket/src/main/java/org/springframework/boot/autoconfigure/rocket/RocketProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.boot.convert.DurationUnit;
 import org.springframework.rocket.client.ClientProperties;
 import org.springframework.rocket.client.ProducerProperties;
+import org.springframework.rocket.client.PushConsumerProperties;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
@@ -32,12 +33,29 @@ public class RocketProperties {
     private final Producer producer = new Producer();
 
     /**
+     * consumer properties
+     */
+    private final Consumer consumer = new Consumer();
+
+    /**
+     * common listener properties
+     */
+    private final Listener listener = new Listener();
+
+    /**
      * common template properties
      */
     private final Template template = new Template();
 
     public Properties buildProducerProperties() {
         Properties result = this.getProducer().buildProperties();
+        this.getProperties().forEach(result::putIfAbsent);
+        result.putIfAbsent(ClientProperties.NAME_SERVER, nameServer);
+        return result;
+    }
+
+    public Properties buildPushConsumerProperties() {
+        Properties result = this.getConsumer().getPush().buildProperties();
         this.getProperties().forEach(result::putIfAbsent);
         result.putIfAbsent(ClientProperties.NAME_SERVER, nameServer);
         return result;
@@ -69,12 +87,60 @@ public class RocketProperties {
     }
 
     @Data
+    public static class Consumer {
+        private final Push push = new Push();
+
+        @Data
+        public static class Push {
+            private String groupId;
+            private String messageModel;
+            private Integer pullBatchSize;
+            private String allocateMessageQueueStrategy;
+            private Integer minConsumeThreads;
+            private Integer maxConsumeThreads;
+            private Integer consumeBatchSize;
+            @DurationUnit(ChronoUnit.MINUTES)
+            private Duration consumeTimeout;
+            @DurationUnit(ChronoUnit.MILLIS)
+            private Duration shutdownAwaitTermination;
+            private Integer retries;
+            @DurationUnit(ChronoUnit.MILLIS)
+            private Duration suspendCurrentQueueTime = Duration.ofMillis(1000);
+            private Integer retryDelayLevel;
+
+            public Properties buildProperties() {
+                Properties properties = new Properties();
+                PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
+                map.from(this::getGroupId).to(value -> properties.put(PushConsumerProperties.GROUP_ID, value));
+                map.from(this::getMessageModel).to(value -> properties.put(PushConsumerProperties.MESSAGE_MODEL, value));
+                map.from(this::getPullBatchSize).to(value -> properties.put(PushConsumerProperties.PULL_BATCH_SIZE, value));
+                map.from(this::getAllocateMessageQueueStrategy).to(value -> properties.put(PushConsumerProperties.ALLOCATE_MESSAGE_QUEUE_STRATEGY, value));
+                map.from(this::getMinConsumeThreads).to(value -> properties.put(PushConsumerProperties.MIN_CONSUME_THREADS, value));
+                map.from(this::getMaxConsumeThreads).to(value -> properties.put(PushConsumerProperties.MAX_CONSUME_THREADS, value));
+                map.from(this::getConsumeTimeout).as(Duration::toMinutes).to(value -> properties.put(PushConsumerProperties.CONSUME_TIMEOUT_MINUTES, value));
+                map.from(this::getShutdownAwaitTermination).as(Duration::toMillis).to(value -> properties.put(PushConsumerProperties.SHUTDOWN_AWAIT_TERMINATION_MILLIS, value));
+                map.from(this::getRetries).to(value -> properties.put(PushConsumerProperties.RETRIES, value));
+                map.from(this::getSuspendCurrentQueueTime).as(Duration::toMillis).to(value -> properties.put(PushConsumerProperties.SUSPEND_CURRENT_QUEUE_TIME_MILLIS, value));
+                map.from(this::getRetryDelayLevel).to(value -> properties.put(PushConsumerProperties.RETRY_DELAY_LEVEL, value));
+                return properties;
+            }
+        }
+    }
+
+    @Data
+    public static class Listener {
+        /**
+         * Whether listener is concurrency
+         */
+        private Boolean concurrency;
+    }
+
+    @Data
     public static class Template {
         /**
          * Default destination to which messages are sent.
          */
         private String defaultDestination;
-
     }
 
 }
