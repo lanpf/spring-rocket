7d162a40f97b67f709de35bc8965f0959bd21574
lanpf <lanxiaozhu2007@hotmail.com>
2025-03-17 16:24:27 +0800

    rename: 1.3.2

--- spring-rocket/src/main/java/org/springframework/rocket/transaction/TransactionChecker.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.rocket.transaction;
-
-import org.springframework.messaging.Message;
-
-public interface TransactionChecker {
-
-    TransactionState check(Message<?> message);
-}
--- spring-rocket/src/main/java/org/springframework/rocket/transaction/TransactionListener.java
+++ spring-rocket/src/main/java/org/springframework/rocket/transaction/TransactionListener.java
@@ -2,7 +2,9 @@ package org.springframework.rocket.transaction;
 
 import org.springframework.messaging.Message;
 
-public interface TransactionListener extends TransactionChecker {
+public interface TransactionListener {
 
     TransactionState execute(Message<?> message, Object arg);
+
+    TransactionState check(Message<?> message);
 }
